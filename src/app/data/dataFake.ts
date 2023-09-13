9dec33d4588655e49b8d48d4b4562ee735f6390f
riquelmy <riquelmyor@gmail.com>
2023-09-13 10:43:48 -0300

    Updated the content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,27 +1,87 @@
 export const dataFake = [
 {
  "id": "1",
- "photo": "https://miro.medium.com/v2/resize:fit:4398/1*PuthyBD2JKLeDA6LYtmd2A.jpeg",
- "title": "nome da notícia?",
- "description": "olá!"
+ "photo": "https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/05/the-matrix-code-keanu-reeves.jpeg",
+ "title": `A filosofia no filme "Matrix"`,
+ "description": `O filme Matrix é um clássico da ficção científica que explora temas filosóficos profundos, mas também é um filme divertido e cheio de ação. Afinal, quem não gosta de ver Neo voando pelos ares e dando porrada em agentes Smith?
+
+ Uma das coisas que torna Matrix tão divertido é a maneira como ele brinca com as ideias filosóficas. Por exemplo, o mito da caverna, de Platão, é uma referência clara ao filme. No mito, Sócrates descreve uma caverna onde prisioneiros estão acorrentados desde a infância, de frente para uma parede na qual sombras de objetos reais são projetadas por uma fogueira. Os prisioneiros acreditam que as sombras são a própria realidade.
+
+ Em Matrix, o mundo que Neo conhece é uma simulação criada pelas máquinas para controlar a humanidade. Neo é o prisioneiro que é libertado da caverna e vê o mundo real. Essa metáfora é uma maneira divertida e interessante de explorar a ideia de que a realidade pode ser uma ilusão.
+
+ Além disso, Matrix é um filme cheio de ação e efeitos especiais. Isso ajuda a manter o espectador entretido e envolvido na história. Afinal, quem não gosta de ver uma boa luta de kung fu ou uma perseguição de carros?
+
+ No final, Matrix é um filme que agrada tanto aos fãs de filosofia quanto aos fãs de ação. É um filme que vai fazer você pensar, mas também vai fazer você se divertir.`
 },
 {
   "id": "2",
-  "photo": "link2",
-  "title": "terríveltitulo",
-  "description": "kkkkkkk"
+  "photo": "https://imgeng.jagran.com/images/2023/sep/OnePieceLiveAction1694326135927.jpg",
+  "title": `Como a live action de One Piece se compara ao mangá?`,
+  "description": `Opa, Zgeeks!
+
+  A live action de One Piece, produzida pela Netflix, estreou em agosto de 2023 e foi um sucesso de crítica e público. A série adaptou a Saga East Blue, que cobre os primeiros 106 capítulos do mangá.
+
+  Eu, como fã de One Piece desde criança, estava bem animado para assistir a live action. E, para ser sincero, fiquei bem impressionado com o resultado.
+
+  Os efeitos especiais são, sem dúvida, um dos pontos fortes da série. As cenas de ação são emocionantes e realistas, e o mundo de One Piece é trazido à vida de forma impressionante.
+
+  O elenco também é muito elogiado, com Iñaki Godoy interpretando Luffy de forma carismática e divertida. Os outros personagens também são bem interpretados, e o elenco tem química entre si.
+
+  Quanto à fidelidade ao material original, a live action é bastante fiel à história do mangá. Claro, algumas mudanças foram feitas, mas elas são sutis e não prejudicam a história.
+
+  No geral, a live action de One Piece é uma adaptação divertida e fiel que os fãs irão apreciar.
+
+  Mas, e você, o que achou da live action?
+
+  Até a próxima!`
  },
  {
   "id": "3",
-  "photo": "link3",
-  "title": "terríveltitulo3",
-  "description": "kkkkkkk"
+  "photo": "https://blog.br.playstation.com/tachyon/sites/4/2023/02/babee9c954509a0d34a67e7d125b679096ef9d79-scaled.jpg?resize=1088%2C612&crop_strategy=smart",
+  "title": "Baldur's Gate 3: o jogo do ano?",
+  "description": `Fala, Zgeeks!
+
+  Baldur's Gate 3, o novo RPG da Larian Studios, foi lançado em agosto de 2023 e já está causando um grande alvoroço. O jogo recebeu críticas positivas da crítica, e os jogadores estão adorando a liberdade e a profundidade da experiência.
+
+  Mas será que Baldur's Gate 3 tem o que é preciso para ser o jogo do ano?
+
+  Bom, eu diria que sim. O jogo tem tudo o que um RPG de sucesso precisa: uma história envolvente, personagens carismáticos, um mundo rico e detalhado, e um sistema de combate tático e desafiador.
+
+  A história de Baldur's Gate 3 é ambientada no plano material de Faerûn, e segue o jogador em sua jornada para impedir o retorno dos illithid, uma raça alienígena que se alimenta do cérebro humano. A história é bem escrita e cheia de reviravoltas, e os personagens são bem desenvolvidos e carismáticos.
+
+  O mundo de Baldur's Gate 3 é um lugar rico e detalhado, cheio de segredos e perigos. O jogador pode explorar uma variedade de ambientes, desde florestas exuberantes a ruínas antigas. O jogo também oferece uma variedade de opções de personalização, permitindo que os jogadores criem seu próprio personagem único.
+
+  O sistema de combate de Baldur's Gate 3 é baseado em turnos e é altamente tático. Os jogadores precisam usar suas habilidades e equipamentos com sabedoria para derrotar seus inimigos. O combate é desafiador, mas recompensante, e é uma das melhores partes do jogo.
+
+  Claro, o jogo ainda está em desenvolvimento, e a Larian Studios promete adicionar mais conteúdo no futuro. Mas, mesmo no estado atual, Baldur's Gate 3 já é um dos melhores RPGs já lançados.
+
+  Então, eu diria que Baldur's Gate 3 tem grandes chances de ser o jogo do ano. O jogo tem tudo o que um RPG de sucesso precisa, e é uma experiência que os fãs do gênero vão adorar.
+
+  Mas, e você, o que acha do jogo?
+
+  Baldur's Gate 3 tem o que é preciso para ser o jogo do ano?
+
+  Vejo vocês em breve!`
  },
  {
   "id": "4",
-  "photo": "link4",
-  "title": "terríveltitulo4",
-  "description": "kkkkkkk"
+  "photo": "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000046189/93d1eafd8fad5fc219251fe40f820da01dee8906ec22bb0f5f82f200ab5a5f58",
+  "title": "Sea of Stars e a ressurreição dos JRPGs",
+  "description": `Sea of Stars, o novo RPG da Sabotage Studio, é uma carta de amor aos JRPGs clássicos dos anos 90. O jogo se inspira em clássicos como Chrono Trigger, Final Fantasy VI, e Earthbound para criar uma experiência que é ao mesmo tempo familiar e nova.
+
+  A história de Sea of Stars é ambientada no mesmo universo de The Messenger, o jogo anterior da Sabotage Studio. O jogo segue a história de dois jovens, Zadia e Valere, que devem unir suas forças para impedir o retorno dos Fleshmancers, uma raça maligna que ameaça destruir o mundo.
+
+  Os personagens de Sea of Stars são bem desenvolvidos e carismáticos. Zadia é uma jovem sonhadora que quer ser uma grande heroína. Valere é um jovem órfão que está procurando seu lugar no mundo. Os outros personagens do jogo também são bem desenvolvidos e contribuem para a história.
+
+  O mundo de Sea of Stars é um lugar rico e detalhado. O jogador pode explorar uma variedade de ambientes, desde florestas exuberantes a ruínas antigas. O jogo também oferece uma variedade de opções de personalização, permitindo que os jogadores criem seu próprio personagem único.
+
+  O sistema de combate de Sea of Stars é baseado em turnos e é altamente tático. Os jogadores precisam usar suas habilidades e equipamentos com sabedoria para derrotar seus inimigos. O combate é desafiador, mas recompensante, e é uma das melhores partes do jogo.
+
+  Sea of Stars é um jogo importante para o gênero JRPG. O jogo é um exemplo de como os desenvolvedores independentes podem criar experiências que rivalizam com os grandes lançamentos AAA. Sea of Stars é um jogo que irá agradar aos fãs do gênero clássico, e que também irá atrair novos jogadores para o mundo dos JRPGs.
+
+  O que você achou de Sea of Stars?
+
+  Você acha que o jogo reviveu o gênero JRPG?`
  },
 
 ]
